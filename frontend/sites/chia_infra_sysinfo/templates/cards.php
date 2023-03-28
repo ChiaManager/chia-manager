18e1f9c258ca1f228bfc9f0d099c3b90ac3f7f57
OLED1 <oliver@edtmair.at>
2023-03-28 10:08:28 +0200

    Code cleanup.

--- frontend/sites/chia_infra_sysinfo/templates/cards.php
+++ frontend/sites/chia_infra_sysinfo/templates/cards.php
@@ -26,7 +26,7 @@
       echo "NOT AUTHENTICATED.";
       exit();
     }
-
+    
     $sysinfos = $all_returned[1];
     $configureable_downtimes = $all_returned[2];
     $found_downtimes = $all_returned[3];
@@ -66,10 +66,7 @@
       include("functions.php");
       
       foreach($sysinfos["data"] AS $nodeid => $sysinfo){
-        /*echo "<pre>";
-        print_r($sysinfo);
-        echo "</pre>";*/
-      if(!array_key_exists("cpu", $sysinfo) && !array_key_exists("memory", $sysinfo) && !array_key_exists("filesystem", $sysinfo)){
+        if(!array_key_exists("cpu", $sysinfo) && !array_key_exists("memory", $sysinfo) && !array_key_exists("filesystem", $sysinfo)){
 ?>
   <div class='row tab-pane fade show <?php echo ($first ? "active" : ""); ?>' id="node-<?php echo $nodeid; ?>" role="tabpanel" aria-labelledby="node-tab-<?php echo $nodeid; ?>"'>
     <?php $first = false; ?>
